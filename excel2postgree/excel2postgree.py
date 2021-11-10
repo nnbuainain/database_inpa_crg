ccf4097146ecce80da1c346f60cb45f060bede2f
Mauregina <mauraregina@gmail.com>
2021-11-09 21:56:59 -0400

    add connection to postgree database
    Signed-off-by: Mauregina <mauraregina@gmail.com>

--- excel2postgree/excel2postgree.py
+++ excel2postgree/excel2postgree.py
@@ -1,5 +1,64 @@
+import psycopg2
+from utils.utils import config, delete_last_line, cleanup
+
+def create_connection():
+    conn = None
+
+    try:
+        # read connection parameters
+        params = config()
+
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        cur = conn.cursor()
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        print(db_version)
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def menu() -> bool:
+    print('### MIGRATE EXCEL TO POSTGREE ###\n')
+    print(f'Choose one option bellow:')
+    print('---------------------------\n'
+          '0 - Exit\n'
+          '1 - Connect database\n'
+          '--------------------------')
+
+    while True:
+        try:
+            option = int(input())
+        except ValueError:
+            print('Invalid option!\n')
+            delete_last_line(num_rows=2, wait=2)
+        else:
+            if (option < 0) or (option > 1):
+                print('Menu doe not have this option! Try again...')
+                delete_last_line(num_rows=2, wait=2)
+            else:
+                cleanup()
+                break
+    #Sair
+    if option == 0:
+        return False
+
+    #Create connection ----------------------------------------------------------------------
+    elif option == 1:
+        cleanup()
+        conn = create_connection()
+        input('\nType something to return to menu... ')
+    return True
+
 def main():
-    pass
+    keep = True
+
+    while keep:
+        cleanup()
+        keep = menu()
 
 if __name__ == "__main__":
     main()
---
+++ excel2postgree/utils/utils.py
@@ -0,0 +1,35 @@
+from configparser import ConfigParser
+import sys, time, os
+
+def delete_last_line(num_rows, wait: int):
+    cursor_up = '\x1b[1A'
+    delete_line = '\x1b[2K'
+
+    time.sleep(wait)
+
+    for _ in range(num_rows):
+        sys.stdout.write(cursor_up)
+        sys.stdout.write(delete_line)
+
+def cleanup():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
